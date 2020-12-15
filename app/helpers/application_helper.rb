7a8a72ed6d7d115b2b66ba01f1083af24e4aec5f
IlyaKokorev <kockorev.ilya@gmail.com>
2020-12-15 12:57:54 +0300

    fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,31 @@
 module ApplicationHelper
-  def user_avatar(user)
-    # TODO: user real avatars
-    'user.png'
-  end
-
-  def bootstrap_class_for flash_type
-    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type] || flash_type.to_s
-  end
-
-  def flash_messages(opts = {})
+  # здесь только доступно во вьюхах!
+  def bootstrap_flash(opts = {})
     flash.each do |msg_type, message|
-      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
-        concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)}", role: "alert") do
+        concat content_tag(:button, '×', class: "close", data: { dismiss: 'alert' })
         concat message
       end)
     end
     nil
+  end
+
+  def user_avatar(user)
+    asset_pack_path('media/images/user.png')
+  end
+
+  def minimum_password_length
+    if @minimum_password_length
+      "Password #{@minimum_password_length} characters minimum"
+    end
+  end
+
+  def fa_icon(icon_class)
+    content_tag 'span', '', class: "fa fa-#{icon_class}"
+  end
+
+  private
+  def bootstrap_class_for(flash_type)
+    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-success" }.stringify_keys[flash_type.to_s] || flash_type.to_s
   end
 end
