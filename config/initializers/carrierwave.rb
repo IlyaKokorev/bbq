57a436d27e705390c32a7a1a31f9436660207aab
IlyaKokorev <kockorev.ilya@gmail.com>
2020-12-23 15:39:19 +0300

    fixed pgoto uploader

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,7 +8,6 @@ if Rails.env.production?
         region:                'us-east-2',
         aws_access_key_id:     ENV['S3_ACCESS_KEY'],
         aws_secret_access_key: ENV['S3_SECRET_KEY'],
-        :path_style            => true
     }
 
     config.fog_directory  = ENV['S3_BUCKET_NAME']
