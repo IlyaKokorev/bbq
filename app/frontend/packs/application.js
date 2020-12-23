3de6927944868ba96251db950eca7ee243793c5e
IlyaKokorev <kockorev.ilya@gmail.com>
2020-12-23 20:50:12 +0300

    added lightbox

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,8 +1,17 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 
+var jQuery = require('jquery')
+
+// include jQuery in global and window scope (so you can access it globally)
+// in your web browser, when you type $('.div'), it is actually refering to global.$('.div')
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
 import '../styles/application'
 import 'bootstrap/dist/js/bootstrap'
-import "../scripts/map"
+import '../scripts/map'
+import '../scripts/box'
+
 
 const images = require.context('../images', true)
