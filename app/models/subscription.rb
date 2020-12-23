365cd9c6488ca19e03ed38aafc54bd01fe633b59
IlyaKokorev <kockorev.ilya@gmail.com>
2020-12-23 11:39:21 +0300

    fixed validation

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -15,8 +15,8 @@ class Subscription < ApplicationRecord
 
   # Или один email может использоваться только один раз (если анонимная подписка)
   validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
-  validate :check_email_for_subs
-  validate :subscribers_is_author?, unless: -> { user.present? }
+  validate :check_email_for_subs, unless: -> { user.present? }
+  validate :subscribers_is_author?
 
   # Если есть юзер, выдаем его имя,
   # если нет – дергаем исходный метод
@@ -32,13 +32,14 @@ class Subscription < ApplicationRecord
 
   private
 
-  def subscribers_is_author?
-    errors.add(:user_email, :self_subscribe)
-  end
-
   def check_email_for_subs
     errors.add(:user_email, :email_present) if
-      user_id.nil? && user_email.presence &&
-        User.find_by(email: user_email).present?
+      User.find_by(email: user_email).present?
+  end
+
+  def subscribers_is_author?
+    if event.user == user
+      errors.add(:user_email, :self_subscribe)
+    end
   end
 end
