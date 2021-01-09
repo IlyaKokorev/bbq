6a9c57fdf6440930f48fe5e8b8f16af29ceb4545
IlyaKokorev <kockorev.ilya@gmail.com>
2021-01-09 13:09:00 +0300

    added icon to photo mailer

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -41,7 +41,7 @@ class PhotosController < ApplicationController
   private
 
   def send_photo_email(event, photo)
-    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email] - [photo.user.email]).uniq
+    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email] - [photo.user&.email]).uniq
 
     all_emails.each do |email|
       EventMailer.photo(event, photo, email).deliver_now
